1d303a1350c13016e78d54ad605756e054ddda5c
pawelmnk <60062062+pawelmnk@users.noreply.github.com>
2020-01-20 10:20:21 +0100

    Ver. 2 4 dices and 1 button added.

--- app/src/main/java/com/example/dice/MainActivity.java
+++ app/src/main/java/com/example/dice/MainActivity.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageViewDice;
+    private ImageView imageViewDice, imageViewDice2, imageViewDice3, imageViewDice4, imageViewDice5 ;
     Random rng = new Random();
 
     @Override
@@ -18,8 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageViewDice = findViewById(R.id.image_view_dice);
-        imageViewDice.setOnClickListener(new View.OnClickListener() {
+        imageViewDice = findViewById(R.id.dice1);
+        imageViewDice2 = findViewById(R.id.dice2);
+        imageViewDice3 = findViewById(R.id.dice3);
+        imageViewDice4 = findViewById(R.id.dice4);
+        imageViewDice5 = findViewById(R.id.dice5);
+
+        Button button = findViewById(R.id.toggle_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 rollDice();
@@ -29,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
      private void rollDice(){
-        int RandomNumber =  rng.nextInt(6) + 1;
+         int RandomNumber1 =  rng.nextInt(6) + 1;
+         int RandomNumber2 =  rng.nextInt(6) + 1;
+         int RandomNumber3 =  rng.nextInt(6) + 1;
+         int RandomNumber4 =  rng.nextInt(6) + 1;
+         int RandomNumber5 =  rng.nextInt(6) + 1;
 
-        switch (RandomNumber){
+        switch (RandomNumber1){
             case 1:
                 imageViewDice.setImageResource(R.drawable.dice1);
                 break;
@@ -50,8 +61,87 @@ public class MainActivity extends AppCompatActivity {
             case 6:
                 imageViewDice.setImageResource(R.drawable.dice6);
                 break;
-
-
         }
+         switch (RandomNumber2) {
+             case 1:
+                 imageViewDice2.setImageResource(R.drawable.dice1);
+                 break;
+             case 2:
+                 imageViewDice2.setImageResource(R.drawable.dice2);
+                 break;
+             case 3:
+                 imageViewDice2.setImageResource(R.drawable.dice3);
+                 break;
+             case 4:
+                 imageViewDice2.setImageResource(R.drawable.dice4);
+                 break;
+             case 5:
+                 imageViewDice2.setImageResource(R.drawable.dice5);
+                 break;
+             case 6:
+                 imageViewDice2.setImageResource(R.drawable.dice6);
+                 break;
+         }
+
+         switch (RandomNumber3) {
+             case 1:
+                 imageViewDice3.setImageResource(R.drawable.dice1);
+                 break;
+             case 2:
+                 imageViewDice3.setImageResource(R.drawable.dice2);
+                 break;
+             case 3:
+                 imageViewDice3.setImageResource(R.drawable.dice3);
+                 break;
+             case 4:
+                 imageViewDice3.setImageResource(R.drawable.dice4);
+                 break;
+             case 5:
+                 imageViewDice3.setImageResource(R.drawable.dice5);
+                 break;
+             case 6:
+                 imageViewDice3.setImageResource(R.drawable.dice6);
+                 break;
+         }
+         switch (RandomNumber4) {
+             case 1:
+                 imageViewDice4.setImageResource(R.drawable.dice1);
+                 break;
+             case 2:
+                 imageViewDice4.setImageResource(R.drawable.dice2);
+                 break;
+             case 3:
+                 imageViewDice4.setImageResource(R.drawable.dice3);
+                 break;
+             case 4:
+                 imageViewDice4.setImageResource(R.drawable.dice4);
+                 break;
+             case 5:
+                 imageViewDice4.setImageResource(R.drawable.dice5);
+                 break;
+             case 6:
+                 imageViewDice4.setImageResource(R.drawable.dice6);
+                 break;
+         }
+         switch (RandomNumber5) {
+             case 1:
+                 imageViewDice5.setImageResource(R.drawable.dice1);
+                 break;
+             case 2:
+                 imageViewDice5.setImageResource(R.drawable.dice2);
+                 break;
+             case 3:
+                 imageViewDice5.setImageResource(R.drawable.dice3);
+                 break;
+             case 4:
+                 imageViewDice5.setImageResource(R.drawable.dice4);
+                 break;
+             case 5:
+                 imageViewDice5.setImageResource(R.drawable.dice5);
+                 break;
+             case 6:
+                 imageViewDice5.setImageResource(R.drawable.dice6);
+                 break;
+         }
      }
 }
